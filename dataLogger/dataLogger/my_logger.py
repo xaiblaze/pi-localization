708fcff605c0656b30254294a4bf5ded68c5426a
Xavier <31713676+xaiblaze@users.noreply.github.com>
2019-04-26 16:51:12 -0400

    Update my_logger.py

--- dataLogger/dataLogger/my_logger.py
+++ dataLogger/dataLogger/my_logger.py
@@ -2,19 +2,27 @@ import datetime
 import requests
 
 class MyLogger:
-  def __init__ (self, host='http://egorv1.herokuapp.com/api/logging/'):
-    self.host = host
+    def __init__ (self, host='http://egorv1.herokuapp.com/api/logging/'):
+        self.host = host
+        self.distance = 0
 
-  def date(self):
-    return datetime.date.today()
+    def date(self):
+        return datetime.date.today()
 
-  def time(self):
-    return datetime.datetime.now().time()
+    def time(self):
+        return datetime.datetime.now().time()
+  
+    def set_distance(self, value):
+        self.distance = value
+    
+    def get_distance(self):
+        return self.distance
 
-  def send(self, packet):
-    json = {
+    def send(self, packet):
+        json = {
             'packet'  : str(packet),
             'date'    : str(self.date()),
-            'time'    : str(self.time()) 
-    }
-    res = requests.post(self.host, data=json)
+            'time'    : str(self.time()),
+            'distance': self.get_distance()
+        }
+        res = requests.post(self.host, data=json)
